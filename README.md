d7c407a707d8a7a38fcd2bb20cac69d1a2cb80b9
ios7jbpro <71101500+ios7jbpro@users.noreply.github.com>
2023-09-30 22:09:57 +0300

    Initial app push

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CinnyUnofficial
  An unofficial mobile Cinny Matrix client.
+This solely exists because the Cinny app under Chrome(or any browser to be real) sucks to use, for such reasons, like swipe to refresh the page. All this app does is load the website in with all required things(JavaScript, cookies, etc.) in default Android WebView for a better experience.
+
+
+SWB file for Sketchware Pro users is also provided.
+
+You need Android 12 SDK to be able to compile this project.
---
+++ app/src/main/java/com/ios7jbpro/cinny/MainActivity.java
@@ -0,0 +1,328 @@
+package com.ios7jbpro.cinny;
+
+import android.Manifest;
+import android.animation.*;
+import android.app.*;
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.*;
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.*;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.*;
+import org.json.*;
+
+public class MainActivity extends Activity {
+	
+	public final int REQ_CD_UPLOADCINNYAPI = 101;
+	
+	private Timer _timer = new Timer();
+	
+	private FrameLayout linear1;
+	private WebView webview1;
+	private LinearLayout linear2;
+	private TextView textview1;
+	
+	private Intent uploadCinnyAPI = new Intent(Intent.ACTION_GET_CONTENT);
+	private TimerTask showFinishButton;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		
+		if (Build.VERSION.SDK_INT >= 23) {
+			if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+				requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
+			} else {
+				initializeLogic();
+			}
+		} else {
+			initializeLogic();
+		}
+	}
+	
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == 1000) {
+			initializeLogic();
+		}
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear1 = findViewById(R.id.linear1);
+		webview1 = findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+		linear2 = findViewById(R.id.linear2);
+		textview1 = findViewById(R.id.textview1);
+		uploadCinnyAPI.setType("*/*");
+		uploadCinnyAPI.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+		
+		//webviewOnProgressChanged
+		webview1.setWebChromeClient(new WebChromeClient() {
+				@Override public void onProgressChanged(WebView view, int _newProgress) {
+					
+				}
+		});
+		
+		webview1.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+		
+		linear2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				finish();
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		linear2.setVisibility(View.GONE);
+		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+			Window w =MainActivity.this.getWindow();
+			w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+			w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS); w.setStatusBarColor(0xFF9E9E9E);
+		}
+		webview1.getSettings().setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath()); 
+		webview1.getSettings().setAllowFileAccess(true);
+		webview1.getSettings().setAppCacheEnabled(true);
+		webview1.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+		webview1.getSettings().setLoadWithOverviewMode(true);
+		webview1.getSettings().setUseWideViewPort(true);
+		webview1.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+		webview1.getSettings().setDomStorageEnabled(true);
+		webview1.getSettings().setSaveFormData(true);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setBuiltInZoomControls(true);
+		webview1.getSettings().setDisplayZoomControls(false);
+		webview1.setDownloadListener(new DownloadListener() {
+			public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+				DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+				String cookies = CookieManager.getInstance().getCookie(url);
+				request.addRequestHeader("cookie", cookies);
+				request.addRequestHeader("User-Agent", userAgent);
+				request.setDescription("Downloading file...");
+				request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+				request.allowScanningByMediaScanner(); request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+				java.io.File aatv = new java.io.File(Environment.getExternalStorageDirectory().getPath() + "/Download");
+				if(!aatv.exists()){if (!aatv.mkdirs()){ Log.e("TravellerLog ::","Problem creating Image folder");}} request.setDestinationInExternalPublicDir("/Download", URLUtil.guessFileName(url, contentDisposition, mimetype));
+				DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+				manager.enqueue(request);
+				showMessage("Downloading File....");
+				//Notif if success
+				BroadcastReceiver onComplete = new BroadcastReceiver() {
+					public void onReceive(Context ctxt, Intent intent) {
+						showMessage("Download Complete!");
+						unregisterReceiver(this);
+					}};
+				registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+			}
+		});
+		
+		webview1.setWebChromeClient(new WebChromeClient() {
+			// For 3.0+ Devices
+			protected void openFileChooser(ValueCallback uploadMsg, String acceptType) { mUploadMessage = uploadMsg; Intent i = new Intent(Intent.ACTION_GET_CONTENT); i.addCategory(Intent.CATEGORY_OPENABLE);
+				i.setType("*/*"); startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+			}
+			// For Lollipop 5.0+ Devices
+			public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+				if (uploadMessage != null) {
+					uploadMessage.onReceiveValue(null);
+					uploadMessage = null; } uploadMessage = filePathCallback; 
+				Intent intent = fileChooserParams.createIntent(); 
+				    intent.setType("*/*");
+				try {
+					startActivityForResult(intent, REQUEST_SELECT_FILE);
+				} catch (ActivityNotFoundException e) {
+					uploadMessage = null; Toast.makeText(getApplicationContext(), "Cannot Open File Chooser", Toast.LENGTH_LONG).show(); return false; }
+				return true; }
+		});
+		webview1.loadUrl("https://app.cinny.in/");
+	}
+	private ValueCallback<Uri> mUploadMessage;
+	public ValueCallback<Uri[]> uploadMessage;
+	public static final int REQUEST_SELECT_FILE = 100;
+	private final static int FILECHOOSER_RESULTCODE = 1;
+	{
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			case REQ_CD_UPLOADCINNYAPI:
+			if (_resultCode == Activity.RESULT_OK) {
+				ArrayList<String> _filePath = new ArrayList<>();
+				if (_data != null) {
+					if (_data.getClipData() != null) {
+						for (int _index = 0; _index < _data.getClipData().getItemCount(); _index++) {
+							ClipData.Item _item = _data.getClipData().getItemAt(_index);
+							_filePath.add(FileUtil.convertUriToFilePath(getApplicationContext(), _item.getUri()));
+						}
+					}
+					else {
+						_filePath.add(FileUtil.convertUriToFilePath(getApplicationContext(), _data.getData()));
+					}
+				}
+			}
+			break;
+			
+			case REQUEST_SELECT_FILE:
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+				if (uploadMessage == null) return; uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(_resultCode, _data)); uploadMessage = null; }
+			break;
+			
+			case FILECHOOSER_RESULTCODE:
+			if (null == mUploadMessage){
+				return; }
+			Uri result = _data == null || _resultCode != RESULT_OK ? null : _data.getData(); mUploadMessage.onReceiveValue(result);
+			mUploadMessage = null;
+			
+			if (true){
+			}
+			else {
+				
+			}
+			break;
+			default:
+			break;
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		linear2.setVisibility(View.VISIBLE);
+		textview1.setText("Quit(2)");
+		showFinishButton = new TimerTask() {
+			@Override
+			public void run() {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						textview1.setText("Quit(1)");
+						showFinishButton = new TimerTask() {
+							@Override
+							public void run() {
+								runOnUiThread(new Runnable() {
+									@Override
+									public void run() {
+										textview1.setText("Quit(0)");
+										showFinishButton = new TimerTask() {
+											@Override
+											public void run() {
+												runOnUiThread(new Runnable() {
+													@Override
+													public void run() {
+														linear2.setVisibility(View.GONE);
+														textview1.setText("Quit");
+													}
+												});
+											}
+										};
+										_timer.schedule(showFinishButton, (int)(1000));
+									}
+								});
+							}
+						};
+						_timer.schedule(showFinishButton, (int)(1000));
+					}
+				});
+			}
+		};
+		_timer.schedule(showFinishButton, (int)(1000));
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
